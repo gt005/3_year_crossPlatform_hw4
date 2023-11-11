3972d1a5553d2a8142df6c2d83193f84e6880d91
gt005 <karimhamid@yandex.ru>
2023-11-11 17:32:12 +0000

    Express помощен в докер

--- db.ts
+++ db.ts
@@ -4,7 +4,7 @@ const mongoUsername = 'root';
 const mongoPassword = 'root';
 
 export const connectToMongo = async () => {
-    mongoose.connect(`mongodb://${mongoUsername}:${mongoPassword}@localhost:27017/hw4_db?authSource=admin`)
+    mongoose.connect(`mongodb://${mongoUsername}:${mongoPassword}@mongo:27017/hw4_db?authSource=admin`)
     .then(() => console.log('Connected to MongoDB'))
     .catch((error) => console.error('Could not connect to MongoDB', error));
 }
